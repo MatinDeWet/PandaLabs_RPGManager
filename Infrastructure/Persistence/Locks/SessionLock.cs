ba5aeb9fc6296082979614d59316345ed47dcd33
Matin de Wet <matindewet116@gmail.com>
2024-10-05 08:22:07 +0200

    update location and session lock, added monsterlock

--- Infrastructure/Persistence/Locks/SessionLock.cs
+++ Infrastructure/Persistence/Locks/SessionLock.cs
@@ -4,12 +4,26 @@
     {
         public override async Task<bool> HasAccess(Session obj, RepositoryOperationEnum operation, int identityId, CancellationToken cancellationToken)
         {
-            var query = from uc in context.Set<UserCampaign>()
+            if (operation == RepositoryOperationEnum.Insert)
+            {
+                var insertQuery = from uc in context.Set<UserCampaign>()
+                                  where
+                                      uc.UserId == identityId
+                                      && uc.CampaignId == obj.CampaignId
+                                      && uc.Role == CampaignRoleEnum.GameMaster
+                                  select uc;
+
+                return await insertQuery.AnyAsync(cancellationToken);
+            }
+
+            var query = from s in context.Set<Session>()
+                        join uc in context.Set<UserCampaign>() on s.CampaignId equals uc.CampaignId
                         where
-                            uc.UserId == identityId
+                            s.Id == obj.Id
+                            && uc.UserId == identityId
                             && uc.CampaignId == obj.CampaignId
                             && uc.Role == CampaignRoleEnum.GameMaster
-                        select uc;
+                        select s;
 
             return await query.AnyAsync(cancellationToken);
         }
